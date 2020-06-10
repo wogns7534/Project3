5c7cc6385c580acfc96438cd403ab058bb697294
Hyeon9mak <jinha3507@gmail.com>
2020-06-10 17:31:13 +0900

    inital commit

--- daITso/routes/index.js
+++ daITso/routes/index.js
@@ -5,20 +5,14 @@ var pool = mysql.createPool({
   connectionLimit: 5,
   host: 'localhost',
   user: 'root',
-  password: '110345',
-  database: 'tutorial'
+  password: '1234',
+  database: 'daITso'
 });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   pool.getConnection(function(err, connection){
-    connection.query('SELECT * FROM board', function(err, rows){
-      if(err) console.error("err : " + err);
-      console.log("rows : " + JSON.stringify(rows));
-
-      res.render('index', {title: 'test', rows: rows});
-      connection.release();
-    });
+     res.render('index', {title: 'test'});
   });
 });
 
