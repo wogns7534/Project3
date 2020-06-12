17ddf6908c6b31d7bd8f0d3368102459d222e67c
wlsgh6346 <wlsgh6247@gmail.com>
2020-06-12 20:02:16 +0900

    join db 연동
    db 회원가입 연동

--- daITso/routes/index.js
+++ daITso/routes/index.js
@@ -1,114 +1,64 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
-var pool = mysql.createPool({
+const { fstat } = require('fs');
+var connection = mysql.createConnection({
   connectionLimit: 50,
   host: 'localhost',
   user: 'root',
   password: '1234',
-  database: 'daITso'
+  database: 'daitso',
+  port: 3306
+});
+
+connection.connect();
+
+/* GET join page. */
+router.get('/join', function(req, res) {
+  console.log('joinjs . path loaded');
+  res.render('join', {
+    title: 'join'
+  });
+});
+
+/* POST join page. */
+router.post('/join', function(req,res, next){
+  console.log('# User join reuqest arrive.');
+  console.log(req.body);
+  var body = req.body;
+  var customer_id = body.customer_id;
+  var customer_passwd = body.customer_passwd;
+  var customer_name = body.customer_name;
+  var customer_address = body.addr1 + " " + body.addr2;
+  var customer_zipcode = body.zip;
+  var customer_phone = body.customer_phone;
+  var customer_email = body.customer_email;
+
+  var query = connection.query('insert into customer (customer_id, customer_passwd, customer_name, customer_address, customer_zipcode, customer_phone,customer_email) values ("'
+                              + customer_id + '","' + customer_passwd + '","'
+                              + customer_name + '","' + customer_address + '","'
+                              + customer_zipcode + '","' + customer_phone + '","'
+                              + customer_email + '")', 
+                              function (err, rows) {
+      if (err) { throw err; }
+      console.log("Data inserted!");
+  });
+  res.redirect('/join_check');
+});
+
+/* GET join_check page. */
+router.get('/join_check', function (req, res, next) {
+  console.log('join_checkjs . path loaded');
+  res.render('join_check', {
+    title: 'join_check'
+  });
 });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  pool.getConnection(function(err, connection){
-     res.render('index', {title: 'test'});
-  });
-});
-router.get('/product-page', function(req, res, next) {
-  pool.getConnection(function(err, connection){
-     res.render('product-page', {title: 'product-page'});
-  });
-});
-router.get('/products', function(req, res, next) {
-  pool.getConnection(function(err, connection){
-     res.render('products', {title: 'products'});
-  });
-});
-router.get('/purchase', function(req, res, next) {
-  pool.getConnection(function(err, connection){
-     res.render('purchase', {title: 'products'});
-  });
-});
-
-router.get('/modify_info_customer', function(req, res, next) {
-  pool.getConnection(function(err, connection){
-     res.render('modify_info_customer', {title: 'test'});
-  });
-});
-
-router.get('/modify_info_seller', function(req, res, next) {
-  pool.getConnection(function(err, connection){
-     res.render('modify_info_seller', {title: 'test'});
-  });
-});
-
-router.get('/view_info_seller', function(req, res, next) {
-  pool.getConnection(function(err, connection){
-     res.render('view_info_seller', {title: 'test'});
-  });
-});
-router.get('/view_info_customer', function(req, res, next) {
-  pool.getConnection(function(err, connection){
-     res.render('view_info_customer', {title: 'test'});
-  });
-});
-router.get('/join_check', function(req, res, next) {
-  pool.getConnection(function(err, connection){
-     res.render('join_check', {title: 'test'});
-  });
-});
-
-router.get('/seller_page', function(req, res, next) {
-  pool.getConnection(function(err, connection) {
-    res.render('seller_page', {
-      title: 'seller_page'
-    });
-  });
-});
-router.get('/shoppingcart', function(req, res, next) {
-  pool.getConnection(function(err, connection) {
-    res.render('shoppingcart', {
-      title: 'shoppingcart'
-    });
-  });
-});
-router.get('/login', function(req, res, next) {
-  pool.getConnection(function(err, connection) {
-    res.render('login', {
-      title: 'login'
-    });
-  });
-});
-
-router.get('/seller_add_product', function(req, res, next) {
-  pool.getConnection(function(err, connection) {
-    res.render('seller_add_product', {
-      title: 'seller_add_product'
-    });
-  });
-});
-router.get('/cpu_product-page', function(req, res, next) {
-  pool.getConnection(function(err, connection) {
-    res.render('cpu_product-page', {
-      title: 'cpu_product-page'
-    });
-  });
-});
-
-router.get('/join', function(req, res, next) {
-  pool.getConnection(function(err, connection) {
-    res.render('join', {
-      title: 'join'
-    });
-  });
-});
-
-router.get('/customer_page', function(req, res, next) {
-  pool.getConnection(function(err, connection) {
-    res.render('customer_page', {
-      title: 'customer_page'
-    });
+router.get('/index', function (req, res, next) {
+  console.log('indexjs . path loaded');
+  res.render('index', {
+    title: 'index'
   });
 });
 module.exports = router;
